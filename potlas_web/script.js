7f35496868cd0b57bfa96b685f5084026a146da7
PosztoHUN <zsolt.posztobanyi@gmail.com>
2026-05-12 17:54:58 +0200

    frissített pótlás

--- potlas_web/script.js
+++ potlas_web/script.js
@@ -1,32 +1,49 @@
-let potlasok = {};
+let vonalak=[0,1,1,0,1,1,0,0,0,0,0,0,0];
+// 70 72 73 74 75 76 77 78 79 80 81 82 83
+let dSzam
 
-async function loadPotlasok() {
+let osszeg=0;
 
-    const response = await fetch("potlasok.json");
+potlas
+grafikon
 
-    potlasok = await response.json();
-
-    console.log(potlasok);
-
-    updateStats();
+function potlas(){
+    //véletlenszám
+    let veletlen1=Math.floor(Math.random()*6)+1;
+    //képnév összerakása
+    let kepnev1="kepek/k"+veletlen1+".png";
+    //kiíratás konzolra
+    console.log(kepnev1);
+    //kiíratás a kép helyére
+    kep1.src=kepnev1;
+    let veletlen2=Math.floor(Math.random()*6)+1;
+    //képnév összerakása
+    let kepnev2="kepek/k"+veletlen2+".png";
+    //kiíratás konzolra
+    console.log(kepnev2);
+    //kiíratás a kép helyére
+    kep2.src=kepnev2;
+ 
+    osszeg=veletlen1+veletlen2;
+    eredmeny.innerHTML="Összesen:"+osszeg; 
 }
 
-function updateStats() {
+let oszlopok = document.querySelectorAll(".oszlop");
+console.log(oszlopok)
 
-    const lines = Object.keys(potlasok);
+function grafikon(){
+    console.log("kirajzolás");
+    // A div méretét módosítjuk a mentes adott elemére. 20 sor
 
-    const oszlopok = document.querySelectorAll(".oszlop");
-
-    lines.forEach((line, index) => {
-
-        const value = potlasok[line];
-
-        const magassag = value * 20;
-
-        oszlopok[index].style.height = `${magassag}px`;
-    });
+    for (i=0;i<vonalak.length;i++){
+        let arany = (200/dSzam.value)*3
+        oszlopok[i].style.height=vonalak[i]*arany+"px";
+    }
 }
 
-loadPotlasok();
-
-setInterval(loadPotlasok, 10000);
+alapoz.onclick=function(){
+    vonalak=[0,0,0,0,0,0,0,0,0,0,0,0,0];
+    for (i=0;i<vonalak.length;i++){
+        oszlopok[i].style.height = 0 + "px"
+    }
+}
